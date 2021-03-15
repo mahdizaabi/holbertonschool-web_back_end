ff53d25440fde3aff49c13ecfed13b59192bcb99
mahdizaabi <mahdi.zaabi@gmail.com>
2021-03-15 16:28:39 +0100

    Complex types - functions
    Complex types - functions

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""function 
+"""
+
+
+from typing import List, Union, Tuple, Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """[summary]
+
+    Args:
+        multiplier (float): [description]
+    """
+
+    def fnx(arg: float):
+        return multiplier * arg
+    return fnx
