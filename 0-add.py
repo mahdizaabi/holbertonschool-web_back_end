ec9932b29822a72c6ca870ed84ace75b69d9b776
mahdizaabi <mahdi.zaabi@gmail.com>
2021-03-15 13:21:50 +0100

    Basic annotations - add
    Basic annotations - add

---
+++ 0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""[summary]
+"""
+def add (a: int, b: float) -> float:
+    """[type-annotated add function]
+
+    Args:
+        a ([int]): [description]
+        b ([float]): [description]
+
+    Returns:
+        [type]: [float]
+    """
+    return a + b
+
