b70a07a6611c7f84fb2b9fe68ea65dc3d22de01a
mahdizaabi <mahdi.zaabi@gmail.com>
2021-03-15 13:55:12 +0100

    Basic annotations - floor
    Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Basic annotations - floor
+"""
+
+import math
+
+
+
+def floor(n:float) -> float:
+    """[summary]
+
+    Args:
+        n (float): [description]
+
+    Returns:
+        float: [description]
+    """
+    return math.floor(n)
+
