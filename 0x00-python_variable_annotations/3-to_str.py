b7bfa2bd901076aca6e4c95d5c43dfd083562db8
mahdizaabi <mahdi.zaabi@gmail.com>
2021-03-15 14:44:37 +0100

    string representation of the float.
     string representation of the float.

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Basic annotations - to string
+"""
+
+
+def to_str(n: float) -> str:
+    """[summary]
+
+    Args:
+        n (float): [argument]
+
+    Returns:
+        str: [string representation of the float]
+    """
+    return repr(n)
+
