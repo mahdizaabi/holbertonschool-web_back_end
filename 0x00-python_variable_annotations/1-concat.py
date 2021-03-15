5a44c0979f562f732f7de25668bcc8e2c9090ce8
mahdizaabi <mahdi.zaabi@gmail.com>
2021-03-15 13:50:22 +0100

    Basic annotations - concat
    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""[Basic annotations - concat]
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """[summary]
+
+    Args:
+        str1 (str1): [description]
+        str2 (str1): [description]
+
+    Returns:
+        str: [description]
+    """
+    return str1 + str2
