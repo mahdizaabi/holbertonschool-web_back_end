5892886bfc9e24f15dac7fff87616d37107588b5
mahdizaabi <mahdi.zaabi@gmail.com>
2021-03-15 16:51:35 +0100

    Let's duck type an iterable object
    Let's duck type an iterable object

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""duck type an iterable object
+"""
+
+
+from typing import List, Union, Tuple, Callable, Iterable, Sequence
+
+
+def element_length(lst: Sequence) -> Tuple[Sequence, int]:
+    """[summary]
+
+    Args:
+        lst (Sequence): [description]
+
+    Returns:
+        Tuple[Sequence, int]: [description]
+    """
+    return [(i, len(i)) for i in lst]
