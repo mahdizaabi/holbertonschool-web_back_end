f7489022532aa86a56d1ccea9141a655b916ff74
mahdizaabi <mahdi.zaabi@gmail.com>
2021-03-15 13:35:28 +0100

    a type-annotated function add
    a type-annotated function add

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python3
-"""[summary]
-"""
-def add (a: int, b: float) -> float:
-    """[type-annotated add function]
+""" a type-annotated function add """
 
-    Args:
-        a ([int]): [description]
-        b ([float]): [description]
 
-    Returns:
-        [type]: [float]
+def add(a: float, b: float) -> float:
+    """
+    a type-annotated function add that
+    takes a float a and a float b as arguments
+    and returns their sum as a float
     """
     return a + b
-
