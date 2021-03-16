fd2bee6f5ad1c332a1e25784396ff3f532d808b9
mahdizaabi <mahdi.zaabi@gmail.com>
2021-03-16 22:30:12 +0100

    yield the first task to complete
    yield the first task to complete

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -9,8 +9,21 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    delay_List = []
-    for i in range(n):
-        c = await wait_random(max_delay)
-        delay_List.append(c)
-    return delay_List
+    """[summary]
+
+    Args:
+        n (int): [description]
+        max_delay (int): [description]
+
+    Returns:
+        List[float]: [description]
+    """
+    tasks = []
+    delay = []
+    for _ in range(n):
+        tasks.append(wait_random(max_delay))
+    for future in asyncio.as_completed(tasks):
+        returnx = await future
+        delay.append(returnx)
+
+    return delay
