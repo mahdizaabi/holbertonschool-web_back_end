fbdd159b95119c2e7e8adee64bd81d5dca91be7f
mahdizaabi <mahdi.zaabi@gmail.com>
2021-03-17 23:14:45 +0100

    measure the run time of 4 concurrent stream data,
    measure the run time of 4 concurrent stream data, with a delay of 10 seconds
        for each data Stream ... data is collected using async comprehension

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -3,14 +3,16 @@
 import asyncio
 import time
 
-async_comprehension = __import__('1-async_comprehension.py').async_comprehension
+async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    """[measure the total runtime and return it.]
+    """
+    measure the run time of 4 concurrent stream data, with a delay of 10 seconds
+    for each data Stream ... data is collected using async comprehension 
     """
     t0 = time.time()
-    fcts = [async_comprehension() for i in range(4)]
-    await asyncio.gather(*fcts)
+    tasks = [async_comprehension() for i in range(4)]
+    await asyncio.gather(*tasks)
 
     return time.time() - t0
