af5c258da223d3b25902eeddcb9d775c5366aed6
mahdizaabi <mahdi.zaabi@gmail.com>
2021-03-17 14:56:06 +0100

    async Comprehension
    async Comprehension

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -8,7 +8,7 @@ from typing import Generator
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[float, None, None]:
+async def async_comprehension() -> list[float]:
     """[summary]
 
     Returns:
