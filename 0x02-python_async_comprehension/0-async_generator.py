77fa251a0388f9abc4997e6ac0a7d633ae744075
mahdizaabi <mahdi.zaabi@gmail.com>
2021-03-17 14:31:35 +0100

    async_generator
    async_generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,7 +7,7 @@ import asyncio
 from typing import Generator
 
 
-async def async_generator() -> Generator[float]:
+async def async_generator() -> Generator[float, None, None]:
     """[summary]
 
     Yields:
