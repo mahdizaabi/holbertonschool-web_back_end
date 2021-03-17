dd3bd46b0f2a858e81402586598c54be741ec846
mahdizaabi <mahdi.zaabi@gmail.com>
2021-03-17 14:48:19 +0100

    async_comprehension
    async_comprehension

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""async_comprehension"""
+
+
+import asyncio
+from typing import Generator
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> Generator[int, None, None]:
+    """[summary]
+
+    Returns:
+        [type]: [description]
+
+    Yields:
+        Generator[int, None, None]: [description]
+    """
+    return [x async for x in async_generator()]
