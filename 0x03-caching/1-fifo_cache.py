a6ae7c292f056f65b18729ecc3223e27caf2aef5
mahdizaabi <mahdi.zaabi@gmail.com>
2021-03-21 19:47:01 +0100

    FIFO Caching Algoritm
    FIFO Caching Algoritm

--- 0x03-caching/1-fifo_cache.py
+++ 0x03-caching/1-fifo_cache.py
@@ -16,10 +16,11 @@ class FIFOCache(BaseCaching):
         """ assign to the dictionary self.cache_data 
         the item value for the key key."""
 
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            keyToRemove = sorted(self.cache_data)[0]
-            print("DISCARD: {}".format(keyToRemove))
-            self.cache_data.pop(keyToRemove)
+        if key not in self.cache_data.keys():
+            if len(self.cache_data) == BaseCaching.MAX_ITEMS:
+                keyToRemove = sorted(self.cache_data)[0]
+                print("DISCARD: {}".format(keyToRemove))
+                self.cache_data.pop(keyToRemove)
         if key is None or item is None:
             return
         self.cache_data[key] = item
