054e08792bb68a8edc2d6876d03037221d2258fe
mahdizaabi <mahdi.zaabi@gmail.com>
2021-03-24 21:30:59 +0100

    2-hypermedia_pagination.py
    2-hypermedia_pagination.py

--- 0x04-pagination/2-hypermedia_pagination.py
+++ 0x04-pagination/2-hypermedia_pagination.py
@@ -29,7 +29,7 @@ class Server:
 
         return self.__dataset
 
-    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+    def get_page(self, page: int = 1, page_size: int = 10) -> Dict:
         """ Paginate the Dataset and return the appropriate page
             of the dataset
         """
