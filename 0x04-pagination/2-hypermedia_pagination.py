fad1ab4f5e282fe35f62952fd982028dc785dc6b
mahdizaabi <mahdi.zaabi@gmail.com>
2021-03-24 21:39:59 +0100

    hypermedia_pagination.py
    hypermedia_pagination.py

--- 0x04-pagination/2-hypermedia_pagination.py
+++ 0x04-pagination/2-hypermedia_pagination.py
@@ -58,6 +58,6 @@ class Server:
             'page': page,
             'data': self.get_page(page, page_size),
             'next_page': page + 1 if max_pages >= page + 1 else None,
-            'prev_page': page - 1,
+            'prev_page': page - 1 if page > 1 else None,
             'total_pages': max_pages
         })
