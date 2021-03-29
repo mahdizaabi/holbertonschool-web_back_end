018405d6d2833397a42cf145de80e5c494719466
mahdizaabi <mahdi.zaabi@gmail.com>
2021-03-29 20:28:21 +0200

    Logger creation
    Logger creation

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -7,9 +7,7 @@ Holberton Web Stack programming Spec ― Back-end
 import re
 import logging
 import os
-import mysql.connector
 from typing import List
-
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
@@ -57,58 +55,18 @@ def filter_datum(fields: List[str],
 
 
 def get_logger() -> logging.Logger:
-    """
-    Instantiates a logging class
+    """[Function that create a Logger]
 
     Returns:
-    -------
-        logging.Logger object
+        [logging.logger]: [a Logger that only log up to logging.INFO level
+                            it has a streamHandler with a customised formatter]
     """
+
     logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
     logger.propagate = False
-    stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(RedactingFormatter(PII_FIELDS))
-    logger.addHandler(stream_handler)
+    logStreamHandler = logging.StreamHandler()
+    logStreamHandler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(logStreamHandler)
+
     return logger
-
-
-def get_db() -> mysql.connector.connection.MySQLConnection:
-    """
-    Instantiates a mysql.connector.connection class
-
-    Returns:
-    -------
-        mysql.connector.connection.MySQLConnection object connector
-    """
-    config = {
-        'user': os.getenv("PERSONAL_DATA_DB_USERNAME", "root"),
-        'password': os.getenv("PERSONAL_DATA_DB_PASSWORD", ""),
-        'host': os.getenv("PERSONAL_DATA_DB_HOST", "localhost"),
-        'database': os.getenv("PERSONAL_DATA_DB_NAME")
-    }
-    connector = mysql.connector.connect(**config)
-    return connector
-
-
-def main():
-    db = get_db()
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM users;")
-    records = []
-    for row in cursor:
-        msg = f"name={row[0]}; email={row[1]}; phone={row[2]}; " \
-              f"ssn={row[3]}; password={row[4]}; ip={row[5]}; " \
-              f"last_login={row[6]}; user_agent={row[7]};"
-        records.append(msg)
-
-    logger = get_logger()
-    for record in records:
-        logger.info(record)
-
-    cursor.close()
-    db.close()
-
-
-if __name__ == "__main__":
-    main()
