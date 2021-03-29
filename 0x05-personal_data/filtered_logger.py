8b543d77a0c3192387bb8f0dbeff7c82fbd27d52
mahdizaabi <mahdi.zaabi@gmail.com>
2021-03-29 05:21:55 +0200

    add
    add

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -18,22 +18,3 @@ def filter_datum(fields: List[str],
         message = re.sub(
             '(?<={:s}=)[^;]*'.format(field), redaction, message, 1)
     return re.sub(";", separator, message)
-
-
-class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
-
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
-
-    def __init__(self, fields):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields
-
-    def format(self, record: logging.LogRecord) -> str:
-        NotImplementedError
-
-        formatted = logging.Formatter(self.FORMAT).format(record)
-        return filter_datum(self.fields, self.__class__.REDACTION, formatted, self.__class__.SEPARATOR)
