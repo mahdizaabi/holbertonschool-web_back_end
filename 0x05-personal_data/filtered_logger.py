32dcde2a797268d990d461373e1f28797d2a2c88
mahdizaabi <mahdi.zaabi@gmail.com>
2021-03-29 05:03:50 +0200

    filtered_logger.py
    filtered_logger.py

---
+++ 0x05-personal_data/filtered_logger.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+0. Regex-ing
+"""
+
+import re
+import logging
+
+
+def filter_datum(fields, redaction, message, separator):
+    """ returns the log message obfuscated """
+
+    for field in fields:
+        message = re.sub(
+            '(?<={:s}=)[^;]*'.format(field), redaction, message, 1)
+    return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
+
+        formatted = logging.Formatter(self.FORMAT).format(record)
+        return filter_datum(self.fields, self.__class__.REDACTION, formatted, self.__class__.SEPARATOR)
+
+
