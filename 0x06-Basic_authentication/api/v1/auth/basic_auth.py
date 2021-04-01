4baa5e790564ffb86d30020f7cf0b89cb7a4de88
mahdi <mahdi.zaabi@gmail.com>
2021-04-01 20:56:07 +0000

    add

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -87,7 +87,6 @@ class BasicAuth(Auth):
             return None
         if user_pwd is None or type(user_pwd) is not str:
             return None
-        print(len(User.all()))
         if len(User.all()) == 0:
             return User(user_email, user_pwd)
 
