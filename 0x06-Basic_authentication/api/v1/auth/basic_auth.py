035cf416b2479ee03d80f882fa34fd0e874cb5cb
mahdizaabi <mahdi.zaabi@gmail.com>
2021-04-01 23:37:43 +0200

    add

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -107,6 +107,6 @@ class BasicAuth(Auth):
                 Authorization)
             decodedb64 = self.decode_base64_authorization_header(extractb64)
             userCredential = self.extract_user_credentials(decodedb64)
-            userInstance = self.user_object_from_credentials(userCredential)
+            userInstance = self.user_object_from_credentials(userCredential[0][1])
             return userInstance
         return None
