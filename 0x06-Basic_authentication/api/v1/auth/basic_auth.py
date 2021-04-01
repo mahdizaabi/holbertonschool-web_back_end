b86a792a12794e0c31b8f7f676cb8f6c6e60c7ec
mahdizaabi <mahdi.zaabi@gmail.com>
2021-04-01 21:11:24 +0200

    User Credential

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -54,3 +54,19 @@ class BasicAuth(Auth):
         if self.isBase64(base64_authorization_header) is False:
             return None
         return base64.b64decode(base64_authorization_header).decode('utf-8')
+
+    def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):
+        """[returns the user email and password from the Base64 decoded value.]
+
+        Args:
+            self ([type]): [description]
+            str ([type]): [description]
+        """
+
+        if decoded_base64_authorization_header is None:
+            return(None, None)
+        if type(decoded_base64_authorization_header) is not str:
+            return (None, None)
+        if ":" not in decoded_base64_authorization_header:
+            return (None, None)
+        return tuple(decoded_base64_authorization_header.split(":"))
