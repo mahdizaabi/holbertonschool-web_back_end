2aac9ef044830cb4007380bab89b47f21b09d585
mahdi <mahdi.zaabi@gmail.com>
2021-04-01 16:18:56 +0000

    add

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,11 +4,9 @@ Module for Authentication
 0x06-Basic_authentication
 holbertonschool-web_back_end
 """
-
-from flask import request
-from typing import List, TypeVar
 from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
+    """ BasicAuthentication class that inherits from Auth"""
     pass
