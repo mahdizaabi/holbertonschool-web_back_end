85399db8e96a96308b1a853c2261f175a62828b8
mahdi <mahdi.zaabi@gmail.com>
2021-04-01 21:17:03 +0000

    add

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -87,9 +87,10 @@ class BasicAuth(Auth):
             return None
         if user_pwd is None or type(user_pwd) is not str:
             return None
-        if User.search({'email': user_email}) == []:
+        try:
+            users = User.search({'email': user_email})
+        except Exception as e:
             return None
-        users = User.search({'email': user_email})
         for user in users:
             if user.is_valid_password(user_pwd):
                 return user
