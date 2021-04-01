b7244b39789a08e6bc049e3555a19aa590b65e17
mahdizaabi <mahdi.zaabi@gmail.com>
2021-04-01 12:39:18 +0200

    add

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -25,13 +25,18 @@ class Auth():
             return True
         if len(excluded_paths) == 0 or excluded_paths is None:
             return True
-        if path in excluded_paths:
-            return False
-        for path in excluded_paths:
-            if not path.endswith('/'):
-                path += '/'
+        new_list = []
+        for exluded_path in excluded_paths:
+            if not exluded_path.endswith('/'):
+                exluded_path += '/'
+            new_list.append(exluded_path)
         if not path.endswith('/'):
-            path += './'
+            path += '/'
+
+        if path not in new_list:
+            return True
+        else:
+            return False
 
     def authorization_header(self, request=None) -> str:
         """[summary]
