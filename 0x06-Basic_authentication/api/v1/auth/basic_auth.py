0fa8849250210480c7b1f55fe319712baa1debbf
mahdizaabi <mahdi.zaabi@gmail.com>
2021-04-01 22:06:53 +0200

    add

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,6 +6,8 @@ holbertonschool-web_back_end
 """
 from api.v1.auth.auth import Auth
 import base64
+from typing import TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -71,3 +73,24 @@ class BasicAuth(Auth):
         if ":" not in decoded_base64_authorization_header:
             return (None, None)
         return tuple(decoded_base64_authorization_header.split(":"))
+
+    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """[summary]
+
+        Args:
+            self ([type]): [description]
+        """
+
+        if user_email is None or type(user_email) is not str:
+            return None
+        if user_pwd is None or type(user_pwd) is not str:
+            return None
+
+        if User.search({'user_email': user_email}) == []:
+            if len(User.all()) != 0:
+                return None
+
+        userx = User.search({'user_email': user_email})[0]
+        if userx.is_valid_password(user_pwd) is False:
+            return None
+        return userx
