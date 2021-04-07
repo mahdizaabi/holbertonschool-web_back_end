f7fc03212fadec00c805f9209108129f7365ec14
mahdi <mahdi.zaabi@gmail.com>
2021-04-07 19:29:53 +0000

    create_session

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -8,6 +8,8 @@ from api.v1.auth.auth import Auth
 import base64
 from typing import TypeVar
 from models.user import User
+import json
+import uuid
 
 
 class SessionAuth(Auth):
@@ -16,4 +18,23 @@ class SessionAuth(Auth):
     Args:
         Auth ([Class]): [auth Class]
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """[Create a new Session, by generating a Session ID
+            and using t]
+
+        Args:
+            user_id (str, optional): [description]. Defaults to None.
+
+        Returns:
+            str: [description]
+        """
+        if user_id is None:
+            return None
+
+        if type(user_id) is not str:
+            return None
+        self.id = str(uuid.uuid4())
+        self.__class__.user_id_by_session_id[self.id] = user_id
+        return self.id
