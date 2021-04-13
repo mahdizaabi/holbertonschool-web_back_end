90e70759368fb6e4142455c476b43273dcf74d37
mahdi <mahdi.zaabi@gmail.com>
2021-04-13 14:48:36 +0000

    ad

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -22,6 +22,9 @@ elif os.getenv("AUTH_TYPE") == "basic_auth":
 elif os.getenv("AUTH_TYPE") == "session_exp_auth":
     from api.v1.auth.session_exp_auth import SessionExpAuth
     auth = SessionExpAuth()
+elif os.getenv("AUTH_TYPE") == "session_db_auth":
+    from api.v1.auth.session_db_auth import SessionDBAuth
+    auth = SessionDBAuth()
 else:
     from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
