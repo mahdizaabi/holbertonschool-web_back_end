77abad7c990ca5df24e9bfd12ba3c76606e8b189
mahdi <mahdi.zaabi@gmail.com>
2021-04-14 16:55:46 +0000

    add

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -54,5 +54,17 @@ def login():
         abort(401)
 
 
+@app.route('/sessions', methods=['DELETE'])
+def logout():
+    """[Log out]
+    """
+    sidCocks = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(sidCocks)
+    if user is None:
+        abort(403)
+    Auth.destroy_session(user.id)
+    return redirect("/")
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")
