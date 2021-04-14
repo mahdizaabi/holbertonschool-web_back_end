ee24c0eb34db2c56d57b66a7923650d2613e9344
mahdi <mahdi.zaabi@gmail.com>
2021-04-14 17:24:10 +0000

    add

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -112,15 +112,9 @@ class Auth:
             return None
 
     def get_user_from_session_id(self, session_id: str) -> str:
-        """[retrieve user by session_id]
-
-        Args:
-            session_id (str): [description]
-
-        Returns:
-            User if user exist, None otherwise
-        """
-
+        """ returns the corresponding user """
+        if not session_id:
+            return None
         try:
             user = self._db.find_user_by(session_id=session_id)
             return user
