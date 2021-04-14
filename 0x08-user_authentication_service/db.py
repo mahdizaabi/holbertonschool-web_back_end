0b95bb4f750a064cb7a519ba737e40e6c38074b2
mahdi <mahdi.zaabi@gmail.com>
2021-04-14 04:31:43 +0000

    add

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -57,7 +57,7 @@ class DB:
             raise NoResultFound
         return user
 
-    def update_user(self, user_id: str, **kwargs) -> None:
+    def update_user(self, user_id: int, **kwargs) -> None:
         """[update_user]
 
         Args:
