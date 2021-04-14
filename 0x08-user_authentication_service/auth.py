3bbb9c31f599af86c507a7d0dcd905fe7e2c4d2e
mahdi <mahdi.zaabi@gmail.com>
2021-04-14 05:10:42 +0000

    add

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -7,7 +7,10 @@ holbertonschool-web_back_end
 
 import bcrypt
 from typing import ByteString
-
+from db import DB
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+from user import User
 
 def _hash_password(password: str) -> str:
     """[Password encryption]
@@ -18,3 +21,30 @@ def _hash_password(password: str) -> str:
     salt = bcrypt.gensalt()
     hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """[summary]
+
+        Args:
+            email ([type]): [description]
+            password ([type]): [description]
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                raise ValueError("User {} already exists".format(user))
+        except NoResultFound:
+            pass
+        except InvalidRequestError:
+            pass
+
+        hashed = _hash_password(password)
+        return self._db.add_user(email, hashed)
