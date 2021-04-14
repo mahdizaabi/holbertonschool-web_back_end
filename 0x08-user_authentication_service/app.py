91786d3a4e8e5f7c951bc2578ba82e2feaf35273
mahdi <mahdi.zaabi@gmail.com>
2021-04-14 05:33:39 +0000

    add

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -14,5 +14,20 @@ def welcome():
     return jsonify({"message": "Bienvenue"})
 
 
+@app.route('/users', methods=['POST'])
+def users():
+    """[summary]
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": "<registered email>",
+                        "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")
