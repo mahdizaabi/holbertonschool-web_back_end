c8fc91108dfdae1a7494453c58ca7c9b8fb728ed
mahdi <mahdi.zaabi@gmail.com>
2021-04-14 02:06:08 +0000

    add

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -1,51 +1,36 @@
 #!/usr/bin/env python3
 """
-DB Module
-0x08. User authentication service
-holbertonschool-web_back_end
+Database class
 """
-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import Session
-
-from user import Base
-from user import User
+from user import Base, User
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 
 class DB:
-    """DB class
-    """
+    """ Database class for SQLAlchemy """
 
-    def __init__(self) -> None:
-        """Initialize a new DB instance
-        """
+    def __init__(self):
+        """ creates engine """
         self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
-    def _session(self) -> Session:
-        """Memoized session object
-        """
+    def _session(self):
+        """ creates a session """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, password: str) -> User:
-        """[saves a new user to the database]
-
-        Args:
-            email ([str]): [email]
-            password ([hash]): [password]
-        """
-        newUser = User()
-
-        newUser.email = email
-        newUser.hashed_password = password
-        self._session.add(newUser)
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """ saves a new user to the database """
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
         self._session.commit()
-        return newUser
+        return 
