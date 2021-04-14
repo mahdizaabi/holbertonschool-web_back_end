fe997df9ced52ece60dc2c6c18451c779982d15a
mahdi <mahdi.zaabi@gmail.com>
2021-04-14 19:36:51 +0000

    add

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -142,3 +142,23 @@ class Auth:
             return None
         except Exception as e:
             return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """[Generate reset password token]
+
+        Args:
+            email (str): [user Email]
+
+        Returns:
+            str: [genrated token]
+        """
+
+        if not email:
+            raise ValueError
+        try:
+            user = self._db.find_user_by(email=email)
+            uuid = _generate_uuid()
+            self._db.update_user(user.id, reset_token=uuid)
+        except Exception as e:
+            raise ValueError
+        return uuid
