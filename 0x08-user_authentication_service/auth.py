a85f86909a2dcd897e004b2c3233ad7b5e660c6b
mahdi <mahdi.zaabi@gmail.com>
2021-04-14 17:16:50 +0000

    add

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -130,6 +130,14 @@ class Auth:
         return user
 
     def destroy_session(self, user_id: int) -> None:
-        """ The method updates the corresponding user’s session ID to None """
-        self._db.update_user(user_id, session_id=None)
-        return None
+        """[Destroy session based on user id]
+
+        Args:
+            user_id (int): [description]
+            return None
+        """
+        try:
+            user = self._db.update_user(user_id, session_id=None)
+            return None
+        except Exception as e:
+            return None
