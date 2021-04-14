1701b6e648dd31103b1163fbff5159cdc9a94448
mahdi <mahdi.zaabi@gmail.com>
2021-04-14 15:52:08 +0000

    add

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -43,54 +43,5 @@ def sessions():
     return response
 
 
-@app.route('/sessions', methods=['DELETE'])
-def logout():
-    """ user logout by destroying the session_id """
-    cookie = request.cookies.get("session_id")
-    user = AUTH.get_user_from_session_id(cookie)
-    if user is None:
-        abort(403)
-    else:
-        AUTH.destroy_session(user.id)
-        return redirect('/')
-
-
-@app.route('/profile', methods=['GET'])
-def profile():
-    """ finds the corresponding user """
-    cookie = request.cookies.get("session_id")
-    user = AUTH.get_user_from_session_id(cookie)
-    if user is None:
-        abort(403)
-    else:
-        return jsonify({"email": user.email}), 200
-
-
-@app.route('/reset_password', methods=['POST'])
-def get_reset_password_token():
-    """ responds with a reset_token """
-    email = request.form.get('email')
-    try:
-        reset_token = AUTH.get_reset_password_token(email)
-        if not reset_token:
-            abort(403)
-        return jsonify({"email": email, "reset_token": reset_token}), 200
-    except ValueError:
-        abort(403)
-
-
-@app.route('/reset_password', methods=['PUT'])
-def update_password():
-    """ updates the user password """
-    email = request.form.get('email')
-    reset_token = request.form.get('reset_token')
-    new_password = request.form.get('new_password')
-    try:
-        AUTH.update_password(reset_token, new_password)
-        return jsonify({"email": email, "message": "Password updated"})
-    except Exception as e:
-        abort(403)
-
-
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")
