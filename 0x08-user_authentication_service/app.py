28ec45c8802e995e1363471d10492c10b28aebe7
mahdi <mahdi.zaabi@gmail.com>
2021-04-14 05:18:17 +0000

    add

---
+++ 0x08-user_authentication_service/app.py
@@ -0,0 +1,22 @@
+"""
+Route module for the API
+"""
+from os import getenv
+from api.v1.views import app_views
+from flask import Flask, jsonify, abort, request
+from flask_cors import (CORS, cross_origin)
+import os
+
+
+@app_views.route('/', methods=['GET'], strict_slashes=False)
+def welcome_home() -> str:
+    """[summary]
+
+    Returns:
+        str: [description]
+    """
+    return jsonify({"message: "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
