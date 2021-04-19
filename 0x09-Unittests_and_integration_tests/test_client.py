bc35f9de54217865fceefa58c43f23b6f5449518
mahdi <mahdi.zaabi@gmail.com>
2021-04-19 17:17:35 +0000

    add

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -1,24 +1,43 @@
 #!/usr/bin/env python3
-""" Module for testing client """
-
-from client import GithubOrgClient
-from fixtures import TEST_PAYLOAD
-from parameterized import parameterized, parameterized_class
-import json
+"""
+test_client.py
+0x09. Unittests and Integration Tests
+holbertonschool-web_back_end
+"""
 import unittest
-from unittest.mock import patch, PropertyMock, Mock
+from utils import access_nested_map, get_json, memoize
+from parameterized import parameterized
+from typing import (
+    Mapping,
+    Sequence,
+    Any,
+    Dict,
+    Callable,
+)
+
+from unittest.mock import patch
+from unittest.mock import Mock, PropertyMock, MagicMock
+from fixtures import TEST_PAYLOAD
+from client import GitHubOrgClient
 
 
-class TestGithubOrgClient(unittest.TestCase):
-    """ Class for Testing Github Org Client """
+class TestGitHubOrgClient(unittest.TestCase):
+    """[TestGitHubOrgClient]
 
-    @parameterized.expand([
-        ('google'),
-        ('abc')
-    ])
+    Args:
+        unittest ([unittest Class]): [Unit test Base class]
+    """
+
+    @parameterized.expand([('google'), ('abc')])
     @patch('client.get_json')
-    def test_org(self, input, mock):
-        """Test that GithubOrgClient.org returns the correct value"""
-        test_class = GithubOrgClient(input)
-        test_class.org()
-        mock.assert_called_once_with(f'https://api.github.com/orgs/{input}')
+    def test_org(self, org, mock_requests):
+        """[testing the GitHubOrgClient.org module]
+        """
+        instance = GitHubOrgClient(org)
+        mock_requests.side_effect = Exception()
+        try:
+            instance.org
+            instance.org()
+        except Exception as e:
+            mock_requests.assert_called_once_with(
+                "https://api.github.com/orgs/{}".format(org))
