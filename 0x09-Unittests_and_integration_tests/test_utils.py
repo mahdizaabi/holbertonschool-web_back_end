617ccc7ad4ff03af060a5a680bbb3fc1bc81f6d4
mahdi <mahdi.zaabi@gmail.com>
2021-04-19 00:31:31 +0000

    Mock HTTP calls

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -5,7 +5,7 @@ access_nested_map Test Module
 holbertonschool-web_back_end
 """
 import unittest
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 from parameterized import parameterized
 from typing import (
     Mapping,
@@ -15,9 +15,12 @@ from typing import (
     Callable,
 )
 
+from unittest.mock import patch
+from unittest.mock import Mock
+
 
 class TestAccessNestedMap(unittest.TestCase):
-    """[TestAccessNestedMap]
+    """[TestAccessNestedMap testing class subclassed from unittest.TestCase]
 
     Args:
         unittest ([UnitTestClass]): [Base class]
@@ -33,22 +36,48 @@ class TestAccessNestedMap(unittest.TestCase):
         """[summary]
 
         Args:
-            name ([str]): [description]
-            nested_map ([type]): [description]
-            path ([type]): [description]
+            nested_map ([Mapping]): [nested mapping]
+            path ([sequence]): []
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([({}, ("a"), KeyError), ({"a": 1},
                                                    ("a", "b"), KeyError)])
     def test_access_nested_map_exception(self, nested_map, path, expected):
-        """[summary]
+        """[test method:assertRaises context manager
+        to test that a KeyError is raised]
 
         Args:
-            nested_map ([type]): [description]
-            path ([type]): [description]
-            expected ([type]): [description]
+            nested_map ([mapping]): [description]
+            path ([Squence]): [description]
+            expected ([KeyError]): [description]
         """
         with self.assertRaises(KeyError) as raises:
             access_nested_map(nested_map, path)
             self.assertEqual(raises.exception.message, KeyError)
+
+
+class TestGetJson(unittest.TestCase):
+    """[summary]
+
+    Args:
+        unittest ([type]): [description]
+    """
+
+    def response(self, payload):
+        """[summary]
+        """
+        response_mock = Mock()
+        response_mock.json.return_value = payload
+        return response_mock
+
+    @parameterized.expand([("http://example.com", {"payload": True},
+                            {"payload": True}),
+                           ("http://holberton.io", {"payload": False},
+                            {"payload": False})])
+    def test_get_json(self, url, payload, expected):
+        """[summary]
+        """
+        with patch('utils.requests') as mock_requests:
+            mock_requests.get.return_value = self.response(payload)
+            self.assertEqual(get_json(url), expected)
