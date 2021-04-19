d30e3f74e749febbe7eb32d5494c20e5d6ed7db6
mahdi <mahdi.zaabi@gmail.com>
2021-04-19 16:47:18 +0000

    add

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -28,6 +28,17 @@ class TestGitHubOrgClient(unittest.TestCase):
         unittest ([unittest Class]): [Unit test Base class]
     """
 
+    @parameterized.expand([
+        ('google'),
+        ('abc')
+    ])
+    @patch('client.get_json')
+    def test_org(self, input, mock):
+        """Test that GithubOrgClient.org returns the correct value"""
+        test_class = GithubOrgClient(input)
+        test_class.org()
+        mock.assert_called_once_with(f'https://api.github.com/orgs/{input}')
+
     @parameterized.expand([('google'), ('abc')])
     @patch('client.get_json')
     def test_org(self, org, mock_requests):
