563c4bdc31cd138ee7ef1834ff53ca00c3e86233
mahdi <mahdi.zaabi@gmail.com>
2021-04-20 15:09:37 +0000

    Integration test

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -97,6 +97,7 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
                          self.org_payload["repos_url"])
         self.assertEqual(test_class.repos_payload, self.repos_payload)
         self.assertEqual(test_class.public_repos(), self.expected_repos)
+        self.assertEqual(test_class.public_repos("sdsd"), [])
         self.mock.assert_called()
 
     def test_public_repos_with_license(self):
