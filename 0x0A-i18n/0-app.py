dbe69eea2ca6514ba1ec5641d475ad7779b6f297
mahdi <mahdi.zaabi@gmail.com>
2021-04-22 15:07:09 +0000

    add

---
+++ 0x0A-i18n/0-app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Route module for the API
+"""
+from os import getenv
+from flask import Flask, render_template
+from flask_cors import (CORS, cross_origin)
+import os
+
+
+app = Flask(__name__, template_folder='templates')
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def welcome_home():
+    """[Render a basic jinja template]
+    """
+    return render_template("0-index.html")
+
+
+if __name__ == "__main__":
+    app.run()
