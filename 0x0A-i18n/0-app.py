cd0007e1740b487276a55711741d8fc24451de3e
mahdi <mahdi.zaabi@gmail.com>
2021-04-22 20:12:33 +0000

    Parametrize templates mandatory

--- 0x0A-i18n/0-app.py
+++ 0x0A-i18n/0-app.py
@@ -3,12 +3,22 @@
 Route module for the API
 """
 from os import getenv
-from flask import Flask, render_template
+from flask import Flask, render_template, Babel
 from flask_cors import (CORS, cross_origin)
 import os
 
 
 app = Flask(__name__, template_folder='templates')
+babel = Babel(app)
+
+
+class Config():
+    """[summary]
+
+    Returns:
+        [type]: [description]
+    """
+    LANGUAGES = ["en", "fr"]
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
