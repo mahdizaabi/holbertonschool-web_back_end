e0bbee61cde1d8037a4c0ad97ee8a32b9c251f24
mahdi <mahdi.zaabi@gmail.com>
2021-04-22 16:09:51 +0000

    add

--- 0x0A-i18n/1-app.py
+++ 0x0A-i18n/1-app.py
@@ -1,33 +1,27 @@
 #!/usr/bin/env python3
-"""
-Route module for the API
-"""
-from os import getenv
+""" Module for trying out Babel i18n """
 from flask_babel import Babel
-from flask_cors import (CORS, cross_origin)
-import os
-
+from flask import Flask, render_template
 
 app = Flask(__name__, template_folder='templates')
 babel = Babel(app)
 
-class Config():
-    """[summary]
 
-    Returns:
-        [type]: [description]
-    """
-    LANGUAGES = ["en", "fr"]
+class Config(object):
+    """ Configuration Class for Babel """
+
+    LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
+
 app.config.from_object(Config)
 
+
 @app.route('/', methods=['GET'], strict_slashes=False)
-def welcome_home():
-    """[Render a basic jinja template]
-    """
-    return render_template("0-index.html")
+def hello_world() -> str:
+    """Renders a Basic Template for Babel Implementation"""
+    return render_template("1-index.html")
 
 
 if __name__ == "__main__":
