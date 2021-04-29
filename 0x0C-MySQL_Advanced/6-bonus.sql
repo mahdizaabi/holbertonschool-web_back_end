e8c647dc328bae5379c8256e3803e94482d5d3e3
mahdi <mahdi.zaabi@gmail.com>
2021-04-29 21:13:28 +0000

    add

---
+++ 0x0C-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,20 @@
+-- 5. Email validation to sent
+-- a trigger to resets the attribute valid_email only when the email has been changed
+
+DELIMITER !!
+CREATE PROCEDURE AddBonus
+(
+    user_id int,
+    project_name varchar(255),
+    score float
+)
+
+BEGIN
+    IF (SELECT COUNT(*) FROM projects WHERE name = project_name) = 0
+    THEN
+        INSERT INTO projects (name) VALUES (project_name);
+    END IF;
+    INSERT INTO corrections (user_id, project_id, score)
+    VALUES (user_id, project_name, score);
+END!!
+DELIMITER ;
