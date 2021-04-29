86609bc1a3bae5a4ddf568e1de96b04e04f97aef
mahdi <mahdi.zaabi@gmail.com>
2021-04-29 21:15:01 +0000

    add

--- 0x0C-MySQL_Advanced/6-bonus.sql
+++ 0x0C-MySQL_Advanced/6-bonus.sql
@@ -14,6 +14,7 @@ BEGIN
     THEN
         INSERT INTO projects (name) VALUES (project_name);
     END IF;
+     SET project_id = (SELECT id FROM projects WHERE name = project_name LIMIT 1);
     INSERT INTO corrections (user_id, project_id, score)
     VALUES (user_id, project_name, score);
 END!!
