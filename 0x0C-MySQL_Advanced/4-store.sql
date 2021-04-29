0361279567e8efca9c54645a28b5589e32abd104
mahdi <mahdi.zaabi@gmail.com>
2021-04-29 19:31:27 +0000

    add trigger

---
+++ 0x0C-MySQL_Advanced/4-store.sql
@@ -0,0 +1,16 @@
+-- SQL script that creates a table users
+-- In and not out
+
+
+DELIMITER $$
+
+CREATE TRIGGER decrease_after_insert
+    AFTER INSERT ON orders 
+    FOR EACH ROW 
+BEGIN
+    UPDATE items 
+    SET quantity = quantity - 1
+    WHERE name = NEW.item_name 
+END $$
+
+DELIMITER;
