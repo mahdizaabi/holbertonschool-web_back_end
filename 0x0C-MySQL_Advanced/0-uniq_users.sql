1d6c557c3ba0d4906b6eb9b82229fbef2e2da149
mahdi <mahdi.zaabi@gmail.com>
2021-04-29 16:16:44 +0000

    We are all unique!

--- 0x0C-MySQL_Advanced/0-uniq_users.sql
+++ 0x0C-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,6 @@
 -- SQL script that creates a table users
 -- script can be executed on any database
-CREATE DATABASE IF NOT EXISTS holberton;
-USE holberton;
+
 CREATE TABLE IF NOT EXISTS users(
     id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
