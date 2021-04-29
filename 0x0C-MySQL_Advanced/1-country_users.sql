3c60290c44fb16e926648dde961cd42a8cc13271
mahdi <mahdi.zaabi@gmail.com>
2021-04-29 16:23:44 +0000

    in and not out

--- 0x0C-MySQL_Advanced/1-country_users.sql
+++ 0x0C-MySQL_Advanced/1-country_users.sql
@@ -4,6 +4,6 @@
 CREATE TABLE IF NOT EXISTS users(
     id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
+    name VARCHAR(255),
     country ENUM('US', 'CO', 'TN') NOT NULL 
 );
