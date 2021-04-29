be068473db1d77e254f06034cde028c3a5cd6ed6
mahdi <mahdi.zaabi@gmail.com>
2021-04-29 16:56:04 +0000

    load distribution

--- 0x0C-MySQL_Advanced/2-fans.sql
+++ 0x0C-MySQL_Advanced/2-fans.sql
@@ -1,6 +1,7 @@
 -- SQL script that creates a table users
 -- In and not out
 
-SELECT origin as origin, fans as nb_fans
+SELECT origin as origin, SUM(fans) as nb_fans
 FROM metal_bands
-ORDER BY fans;
+GROUP BY origin
+ORDER BY nb_fans DESC;
