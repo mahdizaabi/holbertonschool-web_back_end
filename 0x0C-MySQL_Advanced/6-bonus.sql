7569adc2037b2196046adcb6022a406709ed5c54
mahdi <mahdi.zaabi@gmail.com>
2021-04-29 21:17:13 +0000

    add

--- 0x0C-MySQL_Advanced/6-bonus.sql
+++ 0x0C-MySQL_Advanced/6-bonus.sql
@@ -1,22 +1,21 @@
 -- 5. Email validation to sent
 -- a trigger to resets the attribute valid_email only when the email has been changed
 
-DELIMITER !!
-CREATE PROCEDURE AddBonus
-(
-    user_id int,
-    project_name varchar(255),
-    score float
-)
-
+DROP PROCEDURE IF EXISTS AddBonus;
+DELIMITER $$
+CREATE PROCEDURE AddBonus(
+    user_id INT, 
+    project_name VARCHAR(255), 
+    score FLOAT)
 BEGIN
     DECLARE project_id INT;
     IF (SELECT COUNT(*) FROM projects WHERE name = project_name) = 0
     THEN
         INSERT INTO projects (name) VALUES (project_name);
     END IF;
-     SET project_id = (SELECT id FROM projects WHERE name = project_name LIMIT 1);
-    INSERT INTO corrections (user_id, project_id, score)
-    VALUES (user_id, project_name, score);
-END!!
+    SET project_id = (SELECT id FROM projects WHERE name = project_name LIMIT 1);
+    INSERT INTO corrections (user_id, project_id, score) VALUES(user_id, project_id, score);
+END
+$$
+DELIMITER ;
 DELIMITER ;
