5a9d9645ee7af0ba8fe6895e2b2ab433c63de8ee
mahdi <mahdi.zaabi@gmail.com>
2021-04-29 16:23:16 +0000

    in and not out

---
+++ 0x0C-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- SQL script that creates a table users
+-- In and not out
+
+CREATE TABLE IF NOT EXISTS users(
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+    country ENUM('US', 'CO', 'TN') NOT NULL 
+);
