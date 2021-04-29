b1a97c758000710de82b537e3df4584e31e345a6
mahdi <mahdi.zaabi@gmail.com>
2021-04-29 19:41:33 +0000

    add trigger

--- 0x0C-MySQL_Advanced/4-store.sql
+++ 0x0C-MySQL_Advanced/4-store.sql
@@ -9,8 +9,8 @@ CREATE TRIGGER decrease_after_insert
     FOR EACH ROW 
 BEGIN
     UPDATE items 
-    SET quantity = quantity - 1
-    WHERE item_name = NEW.name 
+    SET quantity = quantity - NEW.number 
+    WHERE name  = NEW.item_name 
 END $$
 
-DELIMITER;
+DELIMITER ;
