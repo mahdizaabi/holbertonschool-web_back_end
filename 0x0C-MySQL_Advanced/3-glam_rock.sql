0d44481610e148249d5b8efe3305bf2dff26ed44
mahdi <mahdi.zaabi@gmail.com>
2021-04-29 17:49:23 +0000

    list bands ranked by their longevity

--- 0x0C-MySQL_Advanced/3-glam_rock.sql
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -8,3 +8,14 @@ END AS lifespan
 FROM metal_bands
 where style LIKE "%Glam rock%"
 ORDER BY lifespan DESC;
+
+-- Second method
+/*
+SELECT
+  band_name AS band_name,
+  IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan
+from
+  metal_bands
+WHERE
+  style LIKE '%Glam rock%'
+  */
