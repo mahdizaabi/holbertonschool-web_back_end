21bcffb660daf46f2b9b6a9a673a02433ad0d867
mahdi <mahdi.zaabi@gmail.com>
2021-05-03 04:14:00 +0000

    add

--- 0x0C-MySQL_Advanced/9-index_name_score.sql
+++ 0x0C-MySQL_Advanced/9-index_name_score.sql
@@ -1,4 +1,4 @@
 -- SQL script that  creates an index idx_name_first_score on the table names and the first letter of name and the score..
--- composite index
+-- Create composite index
 
 CREATE INDEX idx_name_first_score on names (name(1), score);
