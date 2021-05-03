89bd638d06fdf3439b0bc1b46cbcb5fb56c98cb1
mahdi <mahdi.zaabi@gmail.com>
2021-05-03 02:48:08 +0000

    add

--- 0x0C-MySQL_Advanced/8-index_my_names.sql
+++ 0x0C-MySQL_Advanced/8-index_my_names.sql
@@ -1,4 +1,4 @@
--- Optimize simple search
--- creates the first letter of name
-CREATE INDEX idx_name_first
-ON names (name(1));
+-- SQL script that creates an index idx_name_first on the table names and the first letter of name.
+-- string prefix order
+
+CREATE INDEX idx_name_first on names (name(1));
