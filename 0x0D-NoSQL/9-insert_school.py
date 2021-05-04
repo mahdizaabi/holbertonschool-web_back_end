2f6105c3face749923a356c64d4671c49aff02e9
mahdi <mahdi.zaabi@gmail.com>
2021-05-04 16:36:55 +0000

    add

---
+++ 0x0D-NoSQL/9-insert_school.py
@@ -0,0 +1,19 @@
+"""[summary]
+"""
+import pprint
+from pymongo import MongoClient
+
+
+
+def insert_school(mongo_collection, **kwargs):
+    """[summary]
+
+    Args:
+        mongo_collection ([type]): [description]
+    """
+    document = {}
+
+    for k, v in kwargs.items():
+        document[str(k)] = v
+    id = mongo_collection.insert(document)
+    return id
