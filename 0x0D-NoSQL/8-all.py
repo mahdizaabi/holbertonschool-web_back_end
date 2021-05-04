f3bcec47218d11774c4f0d1d7c21afb082dba644
mahdi <mahdi.zaabi@gmail.com>
2021-05-04 14:44:58 +0000

    add

---
+++ 0x0D-NoSQL/8-all.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""[summary]
+"""
+import pprint
+from pymongo import MongoClient
+
+
+def selectedDb.school.find():
+    """[summary]
+    """
+
+    if selectedDb.school.count() == 0:
+        return []
+    return selectedDb.school.find()
