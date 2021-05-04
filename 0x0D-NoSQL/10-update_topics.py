4e123d4b57bfa9f97f179a928973ac405c1190b0
mahdi <mahdi.zaabi@gmail.com>
2021-05-04 16:59:19 +0000

    add

---
+++ 0x0D-NoSQL/10-update_topics.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""[summary]
+"""
+import pprint
+from pymongo import MongoClient
+
+def update_topics(mongo_collection, name, topics):
+    """[summary]
+
+    Args:
+        mongo_collection ([collection object]): [collection inside my_db database]]
+        name ([school name: filter the docs by the scchool name]): [string]
+        topics ([list of topic to be inserted on the dociment]): [description]
+    """
+    mongo_collection.update({"name":name}, {$set: {"topics":topics}}, {multi: true})
