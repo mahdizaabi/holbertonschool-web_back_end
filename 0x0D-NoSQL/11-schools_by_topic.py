7b6597cc6be146e9678a425155cc8596116def02
mahdi <mahdi.zaabi@gmail.com>
2021-05-04 17:26:24 +0000

    add

---
+++ 0x0D-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""[summary]
+"""
+import pprint
+from pymongo import MongoClient
+
+
+def schools_by_topic(mongo_collection, topic):
+    """[function that returns the list of school having a specific topic]
+
+    Args:
+        mongo_collection ([collection object]): [collection]
+        topic ([string]): [string]
+    """
+
+
+    return mongo_collection.find( { "topic": { "$all": [topic] } } )
