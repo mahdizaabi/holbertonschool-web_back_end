6e3df5f12d6f391ebcebf990e7bf53acbe021c3c
mahdi <mahdi.zaabi@gmail.com>
2021-05-06 22:53:00 +0000

    add

--- 0x0E-ES6_basic/100-createIteratorObject.js
+++ 0x0E-ES6_basic/100-createIteratorObject.js
@@ -1,10 +1,6 @@
 export default function createIteratorObject(report) {
   let iterable = [];
 
-  if (!report.allEmployees || typeof report.allEmployees !== 'object') {
-    return iterable;
-  }
-
   iterable = {
     * [Symbol.iterator]() {
       for (const value of Object.values(report.allEmployees)) {
