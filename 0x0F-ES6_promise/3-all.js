29210d0dd60e903f47a8d6ff52516fcd36cae837
mahdi <mahdi.zaabi@gmail.com>
2021-05-11 20:17:01 +0000

    Handle multiple successful promises

---
+++ 0x0F-ES6_promise/3-all.js
@@ -0,0 +1,13 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  uploadPhoto()
+    .then((response1) => {
+      createUser()
+        .then((response2) => console.log(
+          response1.body,
+          response2.firstName,
+          response2.lastName,
+        ));
+    });
+}
