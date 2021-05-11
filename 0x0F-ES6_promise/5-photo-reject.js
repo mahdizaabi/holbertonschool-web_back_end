635d2214f65916054d4016caf5e6411f33e277f8
mahdi <mahdi.zaabi@gmail.com>
2021-05-11 21:00:41 +0000

    reject the promises

--- 0x0F-ES6_promise/5-photo-reject.js
+++ 0x0F-ES6_promise/5-photo-reject.js
@@ -1,6 +1,6 @@
 export default function uploadPhoto(filename) {
-  const file = filename;
+  
   return new Promise((resolve, reject) => {
-    reject(new Error(`${file} cannot be processed`));
+    reject(new Error(`${filename} cannot be processed`));
   });
 }
