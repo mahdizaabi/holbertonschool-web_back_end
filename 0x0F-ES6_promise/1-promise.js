651b24f0a0fe5dfe5b89fcdedc76a135ee08fbe6
mahdi <mahdi.zaabi@gmail.com>
2021-05-11 19:39:24 +0000

    add

--- 0x0F-ES6_promise/1-promise.js
+++ 0x0F-ES6_promise/1-promise.js
@@ -1,4 +1,11 @@
 export default function getFullResponseFromAPI(success) {
-  if (success) return new Promise((resolve) => resolve({ status: 200, body: 'Success' }));
-  return new Promise((resolve, reject) => reject('The fake API is not working currently'));
+  return new Promise((resolve, reject)=> {
+      if (success)
+        resolve({
+            status: 200,
+            body :'Success' 
+        })
+    else
+        reject("The fake API is not working currently")
+  })
 }
