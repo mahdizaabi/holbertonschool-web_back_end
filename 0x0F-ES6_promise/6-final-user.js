e0d11c6966edd9a7d42e5c4090299c82a26d71ed
mahdi <mahdi.zaabi@gmail.com>
2021-05-11 21:24:56 +0000

    Handle multiple promises

---
+++ 0x0F-ES6_promise/6-final-user.js
@@ -0,0 +1,11 @@
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
+
+
+export default function handleProfileSignup(firstName, lastName, fileName){
+    const Prom1 =signUpUser(firstName, lastName);
+    const Prom2 = uploadPhoto(fileName);
+    return Promise.allSettled([Prom1, Prom2])
+    .then(results=>console.log(results))
+   
+}
