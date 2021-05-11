be7d2373b4e11ea302e2778a8adbef382df1d540
mahdi <mahdi.zaabi@gmail.com>
2021-05-11 20:36:27 +0000

    multiple promises

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
     .then((values) => {
       const { body } = values[0];
       const { firstName } = values[1];
