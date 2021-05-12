254b5657426adf7e9e273826680d4fd93114f1b9
mahdi <mahdi.zaabi@gmail.com>
2021-05-12 02:52:42 +0000

    add

--- 0x0F-ES6_promise/7-load_balancer.js
+++ 0x0F-ES6_promise/7-load_balancer.js
@@ -1,3 +1,3 @@
-export default async function loadBalancer(chinaDownload, USDownload) {
-  return Promise.any([chinaDownload, USDownload]);
+export default function loadBalancer(chinaDownload, USDownload) {
+  return Promise.race([chinaDownload, USDownload]);
 }
