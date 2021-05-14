6629928cfa5ccde470bc29ea48ce2f7a26ace949
mahdi <mahdi.zaabi@gmail.com>
2021-05-14 16:27:27 +0000

    add

---
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,43 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof (name) === 'string') {
+      this._name = name;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(length) {
+    if (typeof length === 'number') {
+      this._length = length;
+    } else {
+      throw new TypeError('Length must be a number');
+    }
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(students) {
+    if (Array.isArray(students) && ((students.find((s) => typeof s !== 'string')) === 'undefined')) {
+      this._students = students;
+    } else {
+      throw new TypeError('students must be an array of Strings');
+    }
+  }
+}
