069ecb1ae6cba02f0a49d96e3452c4f70bb86453
mahdi <mahdi.zaabi@gmail.com>
2021-05-14 18:51:43 +0000

    add

---
+++ 0x10-ES6_classes/4-pricing.js
@@ -0,0 +1,32 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amount) {
+    this._amount = amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency) {
+    this.currency = currency;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
