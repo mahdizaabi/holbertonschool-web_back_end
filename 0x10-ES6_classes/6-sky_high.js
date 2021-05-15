5529b923d05eb5651d61c11a1af80495a1b559e7
mahdi <mahdi.zaabi@gmail.com>
2021-05-15 00:56:45 +0000

    add

--- 0x10-ES6_classes/6-sky_high.js
+++ 0x10-ES6_classes/6-sky_high.js
@@ -3,21 +3,28 @@ import Building from './5-building';
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
+    this.sqft = sqft;
     this.floors = floors;
-  }
-
-  set floors(value) {
-    if (typeof value !== 'number') {
-      throw new TypeError('Floors must be a number');
-    }
-    this._floors = value;
   }
 
   get floors() {
     return this._floors;
   }
 
+  set floors(floors) {
+    this._floors = floors;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(sqft) {
+    this._sqft = sqft;
+  }
+
   evacuationWarningMessage() {
+    this.x = 14;
     return `Evacuate slowly the ${this._floors} floors`;
   }
 }
