e7b3f3de8f4509b05dadc3fcc3a33b959adfc7fb
mahdi <mahdi.zaabi@gmail.com>
2021-05-18 02:10:54 +0000

    add

---
+++ 0x11-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,10 @@
+const x = (arr, city, newGrades) => {
+  const tempObj = {};
+
+  newGrades.forEach((element) => {
+    tempObj[element.studentId] = element;
+  });
+  return arr.map((item) => ({ ...item, grade: (tempObj[item.id] && (tempObj[item.id].grade)) || 'N/A' })).filter((item) => item.location === city);
+};
+
+export default x;
