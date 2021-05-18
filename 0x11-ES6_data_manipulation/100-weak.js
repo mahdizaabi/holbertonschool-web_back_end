b18655c95eb460372d35f6900c0a5410f595dbd1
mahdi <mahdi.zaabi@gmail.com>
2021-05-18 18:58:41 +0000

    add

--- 0x11-ES6_data_manipulation/100-weak.js
+++ 0x11-ES6_data_manipulation/100-weak.js
@@ -3,7 +3,7 @@ export const queryAPI = (obj) => {
   if (!obj.protocol || !obj.name) {
     return;
   }
-  weakMap.set(obj, ((weakMap.get(obj)) + 1) || 0);
+  weakMap.set(obj, (weakMap.get(obj) && (weakMap.get(obj)) + 1) || 0);
   if (weakMap.get(obj) >= 5) {
     throw Error('Endpoint load is high');
   }
