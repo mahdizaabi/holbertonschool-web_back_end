cf9f2cfe618369e1740a352b2191ac2a43945763
mahdi <mahdi.zaabi@gmail.com>
2021-05-18 14:59:13 +0000

    add

--- 0x11-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x11-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,2 +1,2 @@
-export default (m) => m.forEach((v, k) => ((m instanceof Map) && (v === 1)
-&& m.set(k, 100)) || ((m instanceof Map) && (m.set(k, v)))) || 'Cannot process';
+export default (m) => ((m instanceof Map) && (m.forEach((v, k) => ((v === 1)
+&& m.set(k, 100)) || ((m instanceof Map) && (m.set(k, v)))))) || 'Cannot process';
