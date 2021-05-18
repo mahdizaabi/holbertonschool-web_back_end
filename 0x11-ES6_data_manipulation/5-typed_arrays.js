aa6ae311d72647b5d60c1c114a2be1c9d67eea91
mahdi <mahdi.zaabi@gmail.com>
2021-05-18 04:07:09 +0000

    add

--- 0x11-ES6_data_manipulation/5-typed_arrays.js
+++ 0x11-ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,6 @@
 const x = (length, position, value) => {
   /*eslint-disable */
-  (position > length) && ((function n() { throw Error('Position outside range'); }()));
+  (position > length - 1) && ((function n() { throw Error('Position outside range'); }()));
   const buffer = new Array(length);
   const view = new DataView(buffer, 0);
   view.setInt8(position, value);
