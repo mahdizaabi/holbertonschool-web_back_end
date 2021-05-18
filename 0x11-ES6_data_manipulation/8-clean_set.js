b6a7d3f839baadd4ab7fe937530487707f23c0dd
mahdi <mahdi.zaabi@gmail.com>
2021-05-18 13:17:04 +0000

    add

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -1,3 +1,2 @@
 export default (set, start) => (typeof start === 'string' && start.length !== 0 && typeof set === 'object' && [...set]
-  .filter((item) => item && item.startsWith(start))
-  .map((i) => i.slice(start.length)).join('-')) || '';
+  .filter((item) => item && item.startsWith(start)).map((i) => i.slice(start.length)).join('-')) || '';
