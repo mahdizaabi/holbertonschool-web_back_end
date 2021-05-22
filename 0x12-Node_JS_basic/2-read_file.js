5326a5e311373ab8b8dffb0051474bc19b5ad114
mahdi <mahdi.zaabi@gmail.com>
2021-05-22 14:29:15 +0000

    synchronous read

---
+++ 0x12-Node_JS_basic/2-read_file.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+function countStudents(pathFile) {
+  const csList = [];
+  const sweList = [];
+  try {
+    // read contents of the file
+    const data = fs.readFileSync(pathFile, 'UTF-8');
+    const lines = data.split(/\r?\n/);
+    lines.forEach((line) => {
+      if (line.includes('CS')) {
+        csList.push(line.split(',')[0]);
+      } else if (line.includes('SWE')) {
+        sweList.push(line.split(',')[0]);
+      }
+    });
+    const studentsCount = csList.length + sweList.length;
+    console.log(`Number of students: ${studentsCount}`);
+    console.log(`Number of students in CS: ${csList.length}. List: ${csList.join(', ')}`);
+    console.log(`Number of students in SWE: ${sweList.length}. List: ${sweList.join(', ')}`);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+module.exports = countStudents;
