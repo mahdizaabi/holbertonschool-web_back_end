b3b4cdec4fda436628052739a067cd55ab664e70
mahdi <mahdi.zaabi@gmail.com>
2021-05-22 03:32:42 +0000

    add

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -1,5 +1,5 @@
 process.stdin.setEncoding('utf8');
-process.stdout.write('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
   if (chunk !== null) {
