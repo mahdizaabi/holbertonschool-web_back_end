5f45254c907a3d8416dcb6111ae2616a92431742
mahdi <mahdi.zaabi@gmail.com>
2021-05-22 22:40:57 +0000

    complex HTTP server

--- 0x12-Node_JS_basic/5-http.js
+++ 0x12-Node_JS_basic/5-http.js
@@ -1,29 +1,41 @@
 const http = require('http');
-const databaQuery = require('./3-read_file_async');
+const fs = require('fs');
 
 const filename = process.argv[2];
 const app = http.createServer();
 app.on('request', (request, response) => {
+  const csList = [];
+  const sweList = [];
   if (request.method === 'GET' && request.url === '/students') {
-    databaQuery(filename).then(({
-      listCs, nCs, listSwe, nSwe,
-    }) => {
-      const studentsCount = nCs + nSwe;
-      response.writeHead(200, { 'Content-Type': 'text/plain' });
-      response.write('This is the list of our students\n');
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    fs.readFile(filename, 'utf8', (err, data) => {
+      const lines = data.split(/\r?\n/);
+      lines.forEach((line) => {
+        if (line.includes('CS')) {
+          csList.push(line.split(',')[0]);
+        } else if (line.includes('SWE')) {
+          sweList.push(line.split(',')[0]);
+        }
+      });
+      const studentsCount = csList.length + sweList.length;
+      response.write('this is the list of the students\n');
       response.write(`Number of students: ${studentsCount}\n`);
-      response.write(`Number of students in CS: ${nCs}. List: ${listCs}\n`);
-      response.write(`Number of students in SWE: ${nSwe}. List: ${listSwe}\n`);
+      response.write(`Number of students in CS: ${csList.length}. List: ${csList.join(', ')}\n`);
+      response.write(`Number of students in SWE: ${sweList.length}. List: ${sweList.join(', ')}`);
       response.end();
-    }).catch((e) => console.log(e));
+
+      response.on('error', (err) => {
+        console.log(err);
+      });
+    });
   } else {
     const body = 'Hello Holberton School!';
     response
       .writeHead(200, {
+        'Content-Length': Buffer.byteLength(body),
         'Content-Type': 'text/plain',
-      });
-    response.write(body);
-    response.end();
+      })
+      .end(body);
   }
 });
 app.listen(1245);
