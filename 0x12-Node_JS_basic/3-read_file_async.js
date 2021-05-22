70ae67607a08093ab389a2668c990cdf5ae95ef5
mahdi <mahdi.zaabi@gmail.com>
2021-05-22 19:21:50 +0000

    add

--- 0x12-Node_JS_basic/3-read_file_async.js
+++ 0x12-Node_JS_basic/3-read_file_async.js
@@ -1,24 +1,29 @@
 const readFile = require('fs/promises');
 
 function countStudents(fileName) {
-  const csList = [];
-  const sweList = [];
-
-  return readFile.readFile(fileName, 'UTF-8').then((data) => {
-    const lines = data.split(/\r?\n/);
-    lines.forEach((line) => {
-      if (line.includes('CS')) {
-        csList.push(line.split(',')[0]);
-      } else if (line.includes('SWE')) {
-        sweList.push(line.split(',')[0]);
-      }
-    });
-    const studentsCount = csList.length + sweList.length;
-    console.log(`Number of students: ${studentsCount}`);
-    console.log(`Number of students in CS: ${csList.length}. List: ${csList.join(', ')}`);
-    console.log(`Number of students in SWE: ${sweList.length}. List: ${sweList.join(', ')}`);
-    return Promise.resolve();
-  }).catch(() => Promise.reject(Error('Cannot load the database')));
+  return new Promise((resolve, reject) => {
+    try {
+      const csList = [];
+      const sweList = [];
+      readFile.readFile(fileName, 'UTF-8').then((data) => {
+        const lines = data.split(/\r?\n/);
+        lines.forEach((line) => {
+          if (line.includes('CS')) {
+            csList.push(line.split(',')[0]);
+          } else if (line.includes('SWE')) {
+            sweList.push(line.split(',')[0]);
+          }
+        });
+        const studentsCount = csList.length + sweList.length;
+        console.log(`Number of students: ${studentsCount}`);
+        console.log(`Number of students in CS: ${csList.length}. List: ${csList.join(', ')}`);
+        console.log(`Number of students in SWE: ${sweList.length}. List: ${sweList.join(', ')}`);
+        resolve(data);
+      }).catch(() => reject(new Error('Cannot load the database')));
+    } catch (e) {
+      console.log(e);
+    }
+  });
 }
 
 module.exports = countStudents;
