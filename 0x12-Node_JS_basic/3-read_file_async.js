a346dd5f77b46510d6a65fd26e4caf1fb8193e7b
mahdi <mahdi.zaabi@gmail.com>
2021-05-22 19:54:09 +0000

    read async

--- 0x12-Node_JS_basic/3-read_file_async.js
+++ 0x12-Node_JS_basic/3-read_file_async.js
@@ -1,28 +1,29 @@
-const readFile = require('fs/promises');
+const fs = require('fs');
 
-function countStudents(fileName) {
+function countStudents(path) {
+  const csList = [];
+  const sweList = [];
+
   return new Promise((resolve, reject) => {
-    try {
-      const csList = [];
-      const sweList = [];
-      readFile.readFile(fileName, 'UTF-8').then((data) => {
-        const lines = data.split(/\r?\n/);
-        lines.forEach((line) => {
-          if (line.includes('CS')) {
-            csList.push(line.split(',')[0]);
-          } else if (line.includes('SWE')) {
-            sweList.push(line.split(',')[0]);
-          }
-        });
-        const studentsCount = csList.length + sweList.length;
-        console.log(`Number of students: ${studentsCount}`);
-        console.log(`Number of students in CS: ${csList.length}. List: ${csList.join(', ')}`);
-        console.log(`Number of students in SWE: ${sweList.length}. List: ${sweList.join(', ')}`);
-        resolve(data);
-      }).catch(() => reject(new Error('Cannot load the database')));
-    } catch (e) {
-      console.log(e);
-    }
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(Error('Cannot load the database'));
+        return;
+      }
+      const lines = data.split(/\r?\n/);
+      lines.forEach((line) => {
+        if (line.includes('CS')) {
+          csList.push(line.split(',')[0]);
+        } else if (line.includes('SWE')) {
+          sweList.push(line.split(',')[0]);
+        }
+      });
+      const studentsCount = csList.length + sweList.length;
+      console.log(`Number of students: ${studentsCount}`);
+      console.log(`Number of students in CS: ${csList.length}. List: ${csList.join(', ')}`);
+      console.log(`Number of students in SWE: ${sweList.length}. List: ${sweList.join(', ')}`);
+      resolve();
+    });
   });
 }
 
