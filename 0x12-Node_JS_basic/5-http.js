8da9f1afc83c45b7d78cf90305a37bf3d8c2d00f
mahdi <mahdi.zaabi@gmail.com>
2021-05-23 00:02:58 +0000

    complex server

--- 0x12-Node_JS_basic/5-http.js
+++ 0x12-Node_JS_basic/5-http.js
@@ -1,51 +1,30 @@
 const http = require('http');
-const fs = require('fs');
+const databaQuery = require('./3-read_file_async');
 
 const filename = process.argv[2];
 const app = http.createServer();
 app.on('request', (request, response) => {
-  request.on('error', (err) => {
-    response.statusCode = 404;
-    response.write('This is the list of our students\n');
-    response.end(err.message);
-  });
-
-  const csList = [];
-  const sweList = [];
   if (request.method === 'GET' && request.url === '/students') {
-    try {
-      fs.readFile(filename, 'utf8', (err, data) => {
-        if (err) {
-          response.statusCode = 404;
-          response.end('Cannot load the database');
-          return;
-        }
-        response.writeHead(200, { 'Content-Type': 'text/plain' });
-        const lines = data.split(/\r?\n/);
-        lines.forEach((line) => {
-          if (line.includes('CS')) {
-            csList.push(line.split(',')[0]);
-          } else if (line.includes('SWE')) {
-            sweList.push(line.split(',')[0]);
-          }
-        });
-        const studentsCount = csList.length + sweList.length;
-        response.write('This is the list of our students\n');
-        response.write(`Number of students: ${studentsCount}\n`);
-        response.write(`Number of students in CS: ${csList.length}. List: ${csList.join(', ')}\n`);
-        response.write(`Number of students in SWE: ${sweList.length}. List: ${sweList.join(', ')}`);
-        response.end();
-      });
-    } catch (e) {
-      response.statusCode = 404;
+    databaQuery(filename).then(({
+      listCs, nCs, listSwe, nSwe,
+    }) => {
+      const studentsCount = nCs + nSwe;
+      response.writeHead(200, { 'Content-Type': 'text/plain' });
       response.write('This is the list of our students\n');
-      response.end('Cannot load the database');
-    }
+      response.write(`Number of students: ${studentsCount}\n`);
+      response.write(`Number of students in CS: ${nCs}. List: ${listCs}\n`);
+      response.write(`Number of students in SWE: ${nSwe}. List: ${listSwe}\n`);
+      response.end();
+    }).catch((e) => {
+      response.write('This is the list of our students\n');
+      response.end(e.message);
+    });
   } else {
     const body = 'Hello Holberton School!';
-    response.writeHead(200, {
-      'Content-Type': 'text/plain',
-    });
+    response
+      .writeHead(200, {
+        'Content-Type': 'text/plain',
+      });
     response.write(body);
     response.end();
   }
