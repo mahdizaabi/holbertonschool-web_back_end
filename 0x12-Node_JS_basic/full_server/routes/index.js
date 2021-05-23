4b6acb79f8d05df3b83ed5183d98ac1deeaf636e
mahdi <mahdi.zaabi@gmail.com>
2021-05-23 20:06:36 +0000

    add

--- 0x12-Node_JS_basic/full_server/routes/index.js
+++ 0x12-Node_JS_basic/full_server/routes/index.js
@@ -1,12 +1,15 @@
+import express from 'express';
 import StudentsController from '../controllers/StudentsController';
 import AppController from '../controllers/AppController';
-
-const express = require('express');
 
 const router = express.Router();
 router.get('/', AppController.getHomepage);
 
-router.get('/students', (req, res) => StudentsController.getAllStudents(req, res, process.argv[2]));
-router.get('/students/:major', (req, res) => StudentsController.getAllStudentsByMajor(req, res, process.argv[2]));
+router.get('/students', (req, res) => {
+  StudentsController.getAllStudents(req, res, process.argv[2]);
+});
+router.get('/students/:major', (req, res) => {
+  StudentsController.getAllStudentsByMajor(req, res, process.argv[2]);
+});
 
 export default router;
