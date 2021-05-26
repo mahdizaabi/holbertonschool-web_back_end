5d9bffe7b81835741ab90414b5d9ac5d735c414c
mahdi <mahdi.zaabi@gmail.com>
2021-05-26 01:41:42 +0000

    add

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -6,8 +6,23 @@ const calculateNumber = require('./0-calcul');
 
 
 describe('calculateNumber', function () {
+  const testAddition = ({args, expected}) => function() {
+    assert.strictEqual(calculateNumber(...args), expected);    
+  }
+  it('should return 8 when values are 4 et 2', testAddition({args: [2.1, 6], expected: 8}))
+  it('should return 9 when values are 4 et 2', testAddition({args: [2.8, 6], expected: 9}))
+  it('should return 8 when values are 4 et 2', testAddition({args: [2, 6], expected: 8}))
+  it('should return 8 when values are 4 et 2', testAddition({args: [2, 6], expected: 8}))
+  it('should return 8 when values are 4 et 2', testAddition({args: [14.0, 14.9], expected: 29}))
+  it('should return 8 when values are 4 et 2', testAddition({args: [0.8, 0.8], expected: 2}))
+  it('should return 8 when values are 4 et 2', testAddition({args: [0, 0.2], expected: 0}))
+  it('should return 8 when values are 4 et 2', testAddition({args: [0, 0.2], expected: 0}))
+  it('should return 8 when values are 4 et 2', testAddition({args: [0, 0.2], expected: 0}))
+  it('should return 8 when values are 4 et 2', testAddition({args: [0, 0.2], expected: 0}))
+
+
   it('should return 20 when values are 4 et 2', function () {
-    assert.equal(calculateNumber(4, 2.8), 7);
+    assert.strictEqual(calculateNumber(4, 2.8), 7);
   });
   it('should return NAN when second arg is of type String', function () {
     assert.ok(isNaN(calculateNumber(14, "str")));
