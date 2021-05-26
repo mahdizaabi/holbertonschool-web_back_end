92fd33d305beb0aa99145d83b57f9937efa60cef
mahdi <mahdi.zaabi@gmail.com>
2021-05-26 02:50:58 +0000

    Basic test using Chai assertion library

--- 0x13-unittests_in_js/2-calcul_chai.test.js
+++ 0x13-unittests_in_js/2-calcul_chai.test.js
@@ -1,5 +1,5 @@
 const { assert } = require('chai');
-const calculateNumber = require('./1-calcul');
+const calculateNumber = require('./2-calcul_chai');
 
 describe('Add()', function () {
     const tests = [
