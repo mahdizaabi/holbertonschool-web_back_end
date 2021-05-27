96e1135bc57183e51a8916ab64b52b727a26ba02
mahdi <mahdi.zaabi@gmail.com>
2021-05-27 03:43:17 +0000

    basic integration test

--- 0x13-unittests_in_js/8-api/api.test.js
+++ 0x13-unittests_in_js/8-api/api.test.js
@@ -1,19 +1,56 @@
 const request = require('request');
+const { assert } = require('chai');
 const { expect } = require('chai');
 
-describe('Integration Testing', () => {
-  describe('GET /', () => {
-    it('Code: 200 | Body: Welcome to the payment system', (done) => {
-      const params = {
-        url: 'http://localhost:7865',
-        method: 'GET',
-      };
+const serverUrl = 'http://localhost:7865'
 
-      request(params, function (error, response, body) {
-        expect(response.statusCode).to.equal(200);
-        expect(body).to.equal('Welcome to the payment system');
-        done();
-      });
+function fetchServer(serverUrl) {
+    return new Promise((resolve, reject) => {
+        request(serverUrl, (e, res, body) => {
+            if (e) {
+                resolve({ res, body, e })
+            }
+            else {
+                reject(e)
+            }
+        })
+    })
+}
+
+describe('Basic Integration testing', function () {
+    describe('test server response after GET request', function () {
+
+        describe('GET /', () => {
+            it('Code: 200 | Body: Welcome to the payment system', (done) => {
+                const options = {
+                    url: 'http://localhost:7865',
+                    method: 'GET',
+                };
+
+                request(options, function (error, response, body) {
+                    expect(response.statusCode).to.equal(200);
+                    expect(body).to.equal('Welcome to the payment system');
+                    done();
+                });
+            });
+        });
+
+        it('test the response', function (done) {
+            fetchServer(serverUrl).then(({ response, body }) => {
+                expect(response.statusCode).to.equal(200);
+                expect(body).to.equal('Welcome to the payment system');
+                done();
+            }).catch((error) => done(error))
+        });
+        it('test error after failed request', function (done) {
+            fetchServer('http://localhost:7866').then(({ e }) => {
+                done();
+            }).catch((error) => {
+                done(error)
+                assert.throws(error);
+            })
+        });
     });
-  });
 });
+
+
