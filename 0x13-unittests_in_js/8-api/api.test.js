9dbc3d8ea333a4676af1903a8d91033609ab9ab4
mahdi <mahdi.zaabi@gmail.com>
2021-05-27 03:40:31 +0000

    addd

--- 0x13-unittests_in_js/8-api/api.test.js
+++ 0x13-unittests_in_js/8-api/api.test.js
@@ -1,41 +1,19 @@
 const request = require('request');
-const { assert } = require('chai');
 const { expect } = require('chai');
 
-const serverUrl = 'http://localhost:7865'
+describe('Integration Testing', () => {
+  describe('GET /', () => {
+    it('Code: 200 | Body: Welcome to the payment system', (done) => {
+      const params = {
+        url: 'http://localhost:7865',
+        method: 'GET',
+      };
 
-function fetchServer(serverUrl) {
-    const res = {};
-    return new Promise((resolve, reject) => {
-        request(serverUrl, (e, res, body) => {
-            if (e) {
-                resolve({ res, body, e })
-            }
-            else {
-                reject(e)
-            }
-        })
-    })
-}
-
-describe('Basic Integration testing', function () {
-    describe('test server response after GET request', function () {
-        it('test the response', function (done) {
-            fetchServer(serverUrl).then(({ response, body }) => {
-                expect(response.statusCode).to.equal(200);
-                expect(body).to.equal('Welcome to the payment system');
-                done();
-            }).catch((error) => done(error))
-        });
-        it('test error after failed request', function (done) {
-            fetchServer('http://localhost:7866').then(({ e }) => {
-                done();
-            }).catch((error) => {
-                done(error)
-                assert.throws(error);
-            })
-        });
+      request(params, function (error, response, body) {
+        expect(response.statusCode).to.equal(200);
+        expect(body).to.equal('Welcome to the payment system');
+        done();
+      });
     });
+  });
 });
-
-
