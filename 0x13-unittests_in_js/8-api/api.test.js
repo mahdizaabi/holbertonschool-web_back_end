02735eabcc097a7c505e0b8dd0147da850054169
mahdi <mahdi.zaabi@gmail.com>
2021-05-27 03:29:12 +0000

    basic api integration test

--- 0x13-unittests_in_js/8-api/api.test.js
+++ 0x13-unittests_in_js/8-api/api.test.js
@@ -3,12 +3,12 @@ const { assert } = require('chai');
 
 const serverUrl = 'http://localhost:7865'
 
-function fetchServer() {
+function fetchServer(serverUrl) {
     const res = {};
     return new Promise((resolve, reject) => {
         request(serverUrl, (e, res, body) => {
             if (e) {
-                resolve({ res, body })
+                resolve({ res, body, e })
             }
             else {
                 reject(e)
@@ -21,16 +21,23 @@ describe('Basic Integration testing', function () {
     describe('test server response after GET request', function () {
 
         it('test the response status', function (done) {
-            fetchServer().then(({ response, body }) => {
+            fetchServer(serverUrl).then(({ response, body }) => {
                 assert.strictEqual(response.statusCode, '200');
                 done();
             }).catch((error) => done(error))
         });
         it('test the response body', function (done) {
-            fetchServer().then(({ response, body }) => {
+            fetchServer(serverUrl).then(({ response, body }) => {
                 assert.strictEqual(body, 'Welcome to the payment system');
                 done();
             }).catch((error) => done(error))
+        });
+        it('test error after failed request', function (done) {
+            fetchServer('http://localhost:7866').then(({ e }) => {
+                done();
+            }).catch((error) => { done(error)
+                assert.throws(error);
+            })
         });
     });
 });
