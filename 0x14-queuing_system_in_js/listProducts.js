04bfbd21609233adf08e4b7ba46ed13e0a4a2a22
mahdi <mahdi.zaabi@gmail.com>
2021-06-01 18:14:39 +0000

    list of products

---
+++ 0x14-queuing_system_in_js/listProducts.js
@@ -0,0 +1,27 @@
+export const listProducts = [
+    {
+      itemId: 1,
+      itemName: 'Suitcase 250',
+      price: 50,
+      initialAvailableQuantity: 8,
+    },
+    {
+      itemId: 2,
+      itemName: 'Suitcase 450',
+      price: 100,
+      initialAvailableQuantity: 10,
+    },
+    {
+      itemId: 3,
+      itemName: 'Suitcase 650',
+      price: 350,
+      initialAvailableQuantity: 2,
+    },
+    {
+      itemId: 4,
+      itemName: 'Suitcase 1050',
+      price: 550,
+      initialAvailableQuantity: 5,
+    },
+  ];
+  
