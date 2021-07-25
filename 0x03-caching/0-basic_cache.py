30630ca50a66d8b5295fe4a61cc07c6a701e7b7d
mahdi <mahdi.zaabi@gmail.com>
2021-07-25 14:55:09 +0000

    add

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -1,42 +1,22 @@
-#!/usr/bin/python3
-""" 0-basic_cache.py """
-
+#!/usr/bin/env python3
+""" caching
+"""
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """[A basic caching system class]
-
-    Args:
-        BaseCaching ([class]): [Base Cache classs]
+    """ a basic caching system class
     """
 
-    def __init__(self):
-        super().__init__()
-
-    def put(self, key, value):
-        """[insert data to the cache]
-
-        Args:
-            key ([string]): [description]
-            value ([type]): [description]
+    def put(self, key, item):
+        """ function put
         """
-
-        if key is None or value is None:
-            return
-        self.cache_data[key] = value
+        if key and item:
+            self.cache_data[key] = item
 
     def get(self, key):
-        """[summary]
-
-        Args:
-            key ([string]): [description]
-
-        Returns:
-            [None or value]: [description]
+        """ function get
         """
-
-        if key is None or key not in self.cache_data.keys():
-            return None
-
-        return self.cache_data[key]
+        if key and key in self.cache_data:
+            return self.cache_data[key]
+        return None
